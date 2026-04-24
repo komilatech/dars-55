cdc9a190a8b8bdb52c52ca6d53a183ec67b8f9d0
komilatech <komilatech12@gmail.com>
2026-04-24 13:57:44 +0500

    script.js qo'shildi

---
+++ script.js
@@ -0,0 +1,7 @@
+
+console.log("Dars 55");
+function test() {
+    let data = "swTa6dgaGjrKQ7XWVi76yCQuh4Ptso0k9tmEYxbmynswoXzzwxL06qjsq2pgJ9YuSOasDoroSWwO6g8a5jxzHCr5nqrTr0gVYd04";
+    console.log(data);
+}
+test();
